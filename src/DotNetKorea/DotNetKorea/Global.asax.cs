3a28f328c26c50a5b1a7feb09e8c87e3b0905ef2
Park YongJune <redplus@redplus.net>
2023-12-08 13:23:50 +0900

    .

--- src/DotNetKorea/DotNetKorea/Global.asax.cs
+++ src/DotNetKorea/DotNetKorea/Global.asax.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
-using System.Web.Http;
 
 namespace DotNetKorea
 {
@@ -19,7 +15,7 @@ namespace DotNetKorea
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);            
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
